2db50355b0e74e4b20aba8f63c24cb7eac43dd53
Brett Briggs <bbriggs@ktgames.org>
2024-05-06 09:32:48 -0400

    refactor: changed file names from ExampleConfig, ExamplePlugin, ExamplePluginTest to DeathTracker etc.
    build: added icon.png

--- src/test/java/com/cedolad/DeathTrackerTest.java
+++ src/test/java/com/cedolad/DeathTrackerTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.cedolad;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DeathTrackerTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DeathTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
